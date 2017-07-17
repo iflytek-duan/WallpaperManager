c3b25d65babd990335a7e82e8ff91a8d194fb017
zihao <crazy.zihao@gmail.com>
2017-07-17 16:02:33 +0800

    更新内容：修改设置壁纸方式.

--- app/src/main/java/com/zihao/wallpapermanager/MainActivity.java
+++ app/src/main/java/com/zihao/wallpapermanager/MainActivity.java
@@ -6,8 +6,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
-import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,24 +15,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         try {
-//            WallpaperManager wp = (WallpaperManager) this.getSystemService(Context.WALLPAPER_SERVICE);
-//            Bitmap bmp = BitmapFactory.decodeResource(getResources(),R.drawable.home);
-//            wp.setBitmap(bmp);
+            WallpaperManager wp = (WallpaperManager) this.getSystemService(Context.WALLPAPER_SERVICE);
+            Bitmap bmp = BitmapFactory.decodeResource(getResources(),R.drawable.home);
+            wp.setBitmap(bmp);
             // 设置系统壁纸
-            WallpaperManager instance = WallpaperManager.getInstance(this);
-            WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
-            DisplayMetrics dm = new DisplayMetrics();
-            wm.getDefaultDisplay().getMetrics(dm);
-            int desiredMinimumWidth = dm.widthPixels;
-            int desiredMinimumHeight = dm.heightPixels;
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            instance.suggestDesiredDimensions(desiredMinimumWidth, desiredMinimumHeight);
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.home);
-            instance.setBitmap(bitmap);
+//            WallpaperManager instance = WallpaperManager.getInstance(this);
+//            WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+//            DisplayMetrics dm = new DisplayMetrics();
+//            wm.getDefaultDisplay().getMetrics(dm);
+//            int desiredMinimumWidth = dm.widthPixels;
+//            int desiredMinimumHeight = dm.heightPixels;
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            instance.suggestDesiredDimensions(desiredMinimumWidth, desiredMinimumHeight);
+//            Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.home);
+//            instance.setBitmap(bitmap);
         }catch (Exception e){
             //
         }
